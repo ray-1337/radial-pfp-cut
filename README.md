7373d401c6ed68c499690a5a5058226799a7cd62
ray <33544674+ray-1337@users.noreply.github.com>
2024-12-21 02:37:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Next.js (Personal Template)
+This is my personal Next.js template, designated for me and already used in many and future projects.
---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: false,
+  compress: true,
+  poweredByHeader: false
+};
+
+export default nextConfig;
